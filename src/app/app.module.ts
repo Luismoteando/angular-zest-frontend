2fc8ba3b977affd4a944bcc525a7fb72bca27c48
Luis Miguel Ortiz Rozalén <luismiguel.ortiz.rozalen@alumnos.upm.es>
2021-01-20 23:03:52 +0100

    Generate component hierarchy #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {FooterComponent} from './home/footer/footer.component';
+import {NavigationBarComponent} from './home/navigation-bar/navigation-bar.component';
+import {SchedulerComponent} from './home/scheduler/scheduler.component';
+import {SchedulerHeaderComponent} from './home/scheduler-header/scheduler-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavigationBarComponent,
+    SchedulerComponent,
+    SchedulerHeaderComponent
   ],
   imports: [
     BrowserModule
