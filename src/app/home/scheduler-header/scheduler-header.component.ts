422270ae28e1304e2e6ecc90156675138ac3f013
Luis Miguel Ortiz Rozalén <luismiguel.ortiz.rozalen@alumnos.upm.es>
2021-01-20 23:15:58 +0100

    Generating component hierarchy #4

---
+++ src/app/home/scheduler-header/scheduler-header.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-scheduler-header',
+  templateUrl: './scheduler-header.component.html',
+  styleUrls: ['./scheduler-header.component.css']
+})
+export class SchedulerHeaderComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
