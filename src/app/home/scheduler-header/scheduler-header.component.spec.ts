164709485ed0bb28e1b905c1dbd2955347963e52
Luis Miguel Ortiz Rozalén <luismiguel.ortiz.rozalen@alumnos.upm.es>
2021-01-21 17:48:00 +0100

    Scheduler header component view #10

--- src/app/home/scheduler-header/scheduler-header.component.spec.ts
+++ src/app/home/scheduler-header/scheduler-header.component.spec.ts
@@ -1,17 +1,17 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {SchedulerHeaderComponent} from './scheduler-header.component';
 
-describe('SchedulerHeaderComponent', () => {
+describe('MenusBarComponent', () => {
   let component: SchedulerHeaderComponent;
   let fixture: ComponentFixture<SchedulerHeaderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [SchedulerHeaderComponent]
     })
       .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SchedulerHeaderComponent);
